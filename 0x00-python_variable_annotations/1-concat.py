40fed22a830a2455d1fa53d80df58e21dcf4fbbd
nnejiobioma <nnejiobioma@gmail.com>
2023-10-05 20:14:43 +0000

    task two handled

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotation - adds float
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+     concatenated two floats and return
+     the result as a float.
+    """
+    return str1 + str2

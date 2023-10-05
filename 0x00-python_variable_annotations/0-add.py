78f869677be3455aff164b42263f7431ad916c85
nnejiobioma <nnejiobioma@gmail.com>
2023-10-05 20:05:57 +0000

    task one handled

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a basic annotation
+adds two float
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats and return the result as a float.
+
+    Args:
+        a (float): The first float.
+        b (float): The second float.
+
+    Returns:
+        float: The sum of 'a' and 'b'.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This is project file for 0x00-python_variable_annotations

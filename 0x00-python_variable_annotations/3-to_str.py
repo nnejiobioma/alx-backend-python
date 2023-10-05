4e6ab984eba215705f27645e6d3909a1aeeec080
nnejiobioma <nnejiobioma@gmail.com>
2023-10-05 20:28:26 +0000

    task three handled

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This takes a float
+    
+    Arg:
+    n (float): returns string
+    """
+    return str(n)

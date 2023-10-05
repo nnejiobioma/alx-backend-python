a97f1b543654675bcc824bbf6606bacff84d6f34
nnejiobioma <nnejiobioma@gmail.com>
2023-10-05 20:24:14 +0000

    task three handled

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotation - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Takes n as argumeent.
+
+    Args:
+        n (float): floor.
+
+    Returns:
+        return floor of the number
+    """
+    return int(n)

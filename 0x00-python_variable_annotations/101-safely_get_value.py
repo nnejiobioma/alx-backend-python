b7e25414012f00cc42b44e4b8005b3e848d9db6b
nnejiobioma <nnejiobioma@gmail.com>
2023-10-05 21:17:18 +0000

    made changes to 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
-""" Duck typing - first element of a sequence """
-from typing import Any, Sequence, Union
+"""
+This Type annotated function
+for advance used
+"""
+from typing import Any, Mapping, TypeVar, Union
+T = TypeVar('T')
 
 
-from typing import Dict, TypeVar, Optional
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
+     """
+      Safely get a value from a dictionary.
 
-# Create TypeVars for the key and value types
-K = TypeVar('K')
-V = TypeVar('V')
-
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> V:
-    """
-    Safely get a value from a dictionary.
-
-    Args:
+      Args:
         dct (Dict[K, V]): The dictionary.
         key (K): The key to look up.
-        default (Optional[V]): The default value to return if the key is not found (default is None).
+        default (Optional[V]): The default value to return
+        if the key is not found (default is None).
 
-    Returns:
-        V: The value associated with the key in the dictionary, or the default value if not found.
+      Returns:
+        V: The value associated with the key in the
+        dictionary, or the default value if not found.
     """
     if key in dct:
         return dct[key]
